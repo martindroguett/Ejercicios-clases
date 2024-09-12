0dc26d8580b7d35cc29a811dd6f44c773ff9ff0a
churrasquita <catalina.galleguillos@alumnos.ucn.cl>
2024-09-11 22:34:34 -0400

    Add files via upload

--- D01.py
+++ D01.py
@@ -1,26 +1,36 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 10 22:53:25 2024
+Created on Mon Sep  9 20:41:44 2024
 
-@author: martindroguett
+@author: catalina
 """
-
-x = int(input("Ingrese Número: "))
-count = 0
-suma = 0
-i = 0
-while x <= 1:
-    x = int(input("Debe ser mayor a 1, Reingrese Número: "))
+num = int(input("Ingrese Número: ")) 
+c = 0 # cant de primos
+s = 0 # suma de primos
+while num <= 1:
+    num = int(input("Debe ser mayor a 1, Reingrese Número: "))
 print("Detalle Primos:")
+for i in range(0, (num + 1)):
+     cont = 0
+     for n in range(1, i):
+         if i % n == 0:
+             cont += 1
+     if cont < 2 and i>1:
+         print(i)
+         c += 1
+         s += i
+print(f"Contador primos: {c}")
+print(f"Suma primos: {s}")
+     
+           
 
-for i in range (2, x + 1):
-    for u in range (2, i):
-        if i % u == 0:
-            break
-    else: 
-        print(i)
-        count += 1
-        suma = suma + i
-print(f"Contador primos: {count}")
-print(f"Suma primos: {suma}")
+    
+     
+      
+            
+
+    
+    
+ 
+    
+    
